4f683cd514d979f20da8585028d3114b4d82927e
Andrew C Bates <a@sprock.io>
2023-11-17 08:10:30 -0600

    Add advanced logging
    - Use serilog library for console/file logging
    - Add `--logfile` argument to specify a logfile path
    - Add `--debug` as intermediate logging level
    - Remove elapsed time from peerstats (timestamp is included in the console log line)
    - Log runtime arguments
    - Reformat packet debug log output

--- EchoRelay.Cli/Program.cs
+++ EchoRelay.Cli/Program.cs
@@ -5,9 +5,10 @@ using EchoRelay.Core.Server.Services;
 using EchoRelay.Core.Server.Storage;
 using EchoRelay.Core.Server.Storage.Filesystem;
 using EchoRelay.Core.Utils;
-using Microsoft.AspNetCore.Components.Web;
 using Newtonsoft.Json;
-using System.Reflection;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
 
 namespace EchoRelay.Cli
 {
@@ -69,11 +70,18 @@ namespace EchoRelay.Cli
             [Option("servervalidationtimeout", Required = false, Default = 3000, HelpText = "Sets the timeout for game server validation using raw ping requests. In milliseconds.")]
             public int ServerDBValidateGameServersTimeout { get; set; }
 
-            [Option('v', "verbose", Required = false, Default = false, HelpText = "Verbose output for every message sent between clients and servers.")]
+            [Option('v', "verbose", Required = false, Default = false, HelpText = "Output all data to console/file (includes debug output). ")]
             public bool Verbose { get; set; } = true;
 
-            [Option("disable-cache", Required = false, Default = false, HelpText = "Disable the file cache. Edits to JSON files will be immediately effective.")]
+            [Option('V', "debug", Required = false, Default = false, HelpText = "Output all client/server messages.")]
+            public bool Debug { get; set; } = true;
+
+            [Option('l', "logfile", Required = false, Default = null, HelpText = "Specifies the path to the log file.")]
+            public string? LogFilePath { get; set; }
+
+            [Option("disable-cache", Required = false, Default = false, HelpText = "Disables the file cache. Edits to JSON files will be immediately effective.")]
             public bool DisableCache { get; set; } = true;
+
         }
 
         /// <summary>
@@ -88,23 +96,26 @@ namespace EchoRelay.Cli
                 // Set our options globally
                 Options = options;
 
+                ConfigureLogger(options);
+
                 // Verify the database folder exists.
-                if(!Directory.Exists(options.DatabaseFolder))
+                if (!Directory.Exists(options.DatabaseFolder))
                 {
-                    Error("Provided database folder does not exist. You must specify a valid directory.");
+                    Log.Fatal("Provided database folder does not exist. You must specify a valid directory.");
                     return;
                 }
 
                 // Verify other arguments
                 if (options.Port < 0 || options.Port > ushort.MaxValue)
                 {
-                    Error($"Provided port is invalid. You must a value between 1 and {ushort.MaxValue}.");
+                    Log.Fatal($"Provided port is invalid. You must a value between 1 and {ushort.MaxValue}.");
                     return;
                 }
 
+                Log.Debug($"Runtime arguments: '{string.Join(" ", args)}'");
                 // Create our file system storage and open it.
                 ServerStorage serverStorage = new FilesystemServerStorage(options.DatabaseFolder, Options.DisableCache);
-                
+
                 serverStorage.Open();
 
                 // Check if initial deployment needs to be performed.
@@ -115,7 +126,7 @@ namespace EchoRelay.Cli
                 bool performInitialSetup = !allCriticalResourcesExist;
                 if (performInitialSetup && anyCriticalResourcesExist)
                 {
-                    Warning("Critical resources are missing from storage, but storage is non-empty.\n" +
+                    Log.Warning("Critical resources are missing from storage, but storage is non-empty.\n" +
                         "Would you like to re-deploy initial setup resources? Warning: this will clear all storage except accounts! [y/N]");
                     performInitialSetup = Console.ReadKey(true).Key == ConsoleKey.Y;
                 }
@@ -123,7 +134,7 @@ namespace EchoRelay.Cli
                 // Perform initial setup of server resources if needed.
                 if (performInitialSetup)
                 {
-                    Info("[SERVER] Performing initial setup: server resources to database folder..");
+                    Log.Information("[SERVER] Performing initial setup: server resources to database folder..");
                     InitialDeployment.PerformInitialDeployment(serverStorage, options.GameExecutablePath, false);
                 }
 
@@ -151,7 +162,7 @@ namespace EchoRelay.Cli
                 Server.ServerDBService.OnGameServerRegistrationFailure += ServerDBService_OnGameServerRegistrationFailure;
 
                 // Set up all verbose event handlers.
-                if (options.Verbose)
+                if (options.Debug || options.Verbose)
                 {
                     Server.OnServicePacketSent += Server_OnServicePacketSent;
                     Server.OnServicePacketReceived += Server_OnServicePacketReceived;
@@ -162,15 +173,41 @@ namespace EchoRelay.Cli
             });
         }
 
+        private static void ConfigureLogger(CliOptions options)
+        {
+            var logConfig = new LoggerConfiguration()
+                .WriteTo.Console(theme: AnsiConsoleTheme.Code);
+
+            if (options.LogFilePath != null)
+            {
+                logConfig.WriteTo.File(
+                    path: Options.LogFilePath,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}");
+            }
+
+            if (options.Verbose)
+            {
+                logConfig.MinimumLevel.Verbose();
+            } else if (options.Debug)
+            {
+                logConfig.MinimumLevel.Debug();
+            } else
+            {
+                logConfig.MinimumLevel.Information();
+            }
+
+            Log.Logger = logConfig.CreateLogger();
+        }
+
         private static void Server_OnServerStarted(Server server)
         {
             // Print our server started message
-            Info("[SERVER] Server started");
+            Log.Information("[SERVER] Server started");
 
             // Print our service config.
             Core.Game.ServiceConfig serviceConfig = server.Settings.GenerateServiceConfig(server.PublicIPAddress?.ToString() ?? "localhost", serverConfig: true);
             string serviceConfigSerialized = JsonConvert.SerializeObject(serviceConfig, Formatting.Indented, StreamIO.JsonSerializerSettings);
-            Info($"[SERVER] Generated service config:\n{serviceConfigSerialized}");
+            Log.Information($"[SERVER] Generated service config:\n{serviceConfigSerialized}");
 
             // Copy the service config to the clipboard if required.
             if (Options?.OutputConfigPath != null)
@@ -179,25 +216,25 @@ namespace EchoRelay.Cli
                 try
                 {
                     File.WriteAllText(Options!.OutputConfigPath, serviceConfigSerialized);
-                    Info($"[SERVER] Output generated service config to path \"{Options!.OutputConfigPath}\"");
+                    Log.Information($"[SERVER] Output generated service config to path \"{Options!.OutputConfigPath}\"");
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
-                    Error($"[SERVER] Failed to output generated service config to path \"{Options!.OutputConfigPath}\":\n{ex}");
+                    Log.Error($"[SERVER] Failed to output generated service config to path \"{Options!.OutputConfigPath}\":\n{ex}");
                 }
             }
-
             // Start the peer stats update timer
-            startedTime = DateTime.UtcNow;
-            peerStatsUpdateTimer = new System.Timers.Timer(Options!.StatsUpdateInterval);
-            peerStatsUpdateTimer.Start();
-            peerStatsUpdateTimer.Elapsed += PeerStatsUpdateTimer_Elapsed;
+            if (Options.Verbose)
+            {
+                peerStatsUpdateTimer = new System.Timers.Timer(Options!.StatsUpdateInterval);
+                peerStatsUpdateTimer.Start();
+                peerStatsUpdateTimer.Elapsed += PeerStatsUpdateTimer_Elapsed;
+            }
         }
 
         private static void PeerStatsUpdateTimer_Elapsed(object? sender, System.Timers.ElapsedEventArgs e)
         {
-            Info($"[PEERSTATS] " +
-                $"elapsed: {(DateTime.UtcNow - startedTime)}, " +
+            Log.Verbose($"[PEERSTATS] " +
                 $"gameservers: {Server.ServerDBService.Registry.RegisteredGameServers.Count}, " +
                 $"login: {Server.LoginService.Peers.Count}, " +
                 $"config: {Server.ConfigService.Peers.Count}, " +
@@ -213,102 +250,54 @@ namespace EchoRelay.Cli
             peerStatsUpdateTimer?.Stop();
 
             // Print our server stopped message
-            Info("[SERVER] Server stopped");
+            Log.Information("[SERVER] Server stopped");
+            Log.CloseAndFlush();
         }
 
         private static void Server_OnAuthorizationResult(Server server, System.Net.IPEndPoint client, bool authorized)
         {
-            if(!authorized)
-                Error($"[SERVER] client({client.Address}:{client.Port}) failed authorization");
+            if (!authorized)
+                Log.Information($"[SERVER] client({client.Address}:{client.Port}) failed authorization");
         }
+
         private static void Server_OnServicePeerConnected(Core.Server.Services.Service service, Core.Server.Services.Peer peer)
         {
-            Info($"[{service.Name}] client({peer.Address}:{peer.Port}) connected");
+            Log.Debug($"[{service.Name}] client({peer.Address}:{peer.Port}) connected");
         }
+
         private static void Server_OnServicePeerDisconnected(Core.Server.Services.Service service, Core.Server.Services.Peer peer)
         {
-            Info($"[{service.Name}] client({peer.Address}:{peer.Port}) disconnected");
+            Log.Debug($"[{service.Name}] client({peer.Address}:{peer.Port}) disconnected");
         }
+
         private static void Server_OnServicePeerAuthenticated(Core.Server.Services.Service service, Core.Server.Services.Peer peer, Core.Game.XPlatformId userId)
         {
-        
+            Log.Information($"[{service.Name}] client({peer.Address}:{peer.Port}) authenticated as '{peer.UserDisplayName}'");
         }
 
         private static void Registry_OnGameServerRegistered(Core.Server.Services.ServerDB.RegisteredGameServer gameServer)
         {
-            Info($"[{gameServer.Peer.Service.Name}] client({gameServer.Peer.Address}:{gameServer.Peer.Port}) registered game server (server_id={gameServer.ServerId}, region_symbol={gameServer.RegionSymbol}, version_lock={gameServer.VersionLock}, endpoint=<{gameServer.ExternalAddress}:{gameServer.Port}>)");
+            Log.Information($"[{gameServer.Peer.Service.Name}] client({gameServer.Peer.Address}:{gameServer.Peer.Port}) registered game server (server_id={gameServer.ServerId}, region_symbol={gameServer.RegionSymbol}, version_lock={gameServer.VersionLock}, endpoint=<{gameServer.ExternalAddress}:{gameServer.Port}>)");
         }
 
         private static void Registry_OnGameServerUnregistered(Core.Server.Services.ServerDB.RegisteredGameServer gameServer)
         {
-            Info($"[{gameServer.Peer.Service.Name}] client({gameServer.Peer.Address}:{gameServer.Peer.Port}) unregistered game server (server_id={gameServer.ServerId}, region_symbol={gameServer.RegionSymbol}, version_lock={gameServer.VersionLock}, endpoint=<{gameServer.ExternalAddress}:{gameServer.Port}>)");
-        }
-        private static void ServerDBService_OnGameServerRegistrationFailure(Peer peer, Core.Server.Messages.ServerDB.ERGameServerRegistrationRequest registrationRequest, string failureMessage)
-        {
-            Error($"[{peer.Service.Name}] client({peer.Address}:{peer.Port}) failed to register game server: \"{failureMessage}\"");
+            Log.Information($"[{gameServer.Peer.Service.Name}] client({gameServer.Peer.Address}:{gameServer.Peer.Port}) unregistered game server (server_id={gameServer.ServerId}, region_symbol={gameServer.RegionSymbol}, version_lock={gameServer.VersionLock}, endpoint=<{gameServer.ExternalAddress}:{gameServer.Port}>)");
         }
 
-        private static string getPacketDisplayString(Packet packet)
+        private static void ServerDBService_OnGameServerRegistrationFailure(Peer peer, Core.Server.Messages.ServerDB.ERGameServerRegistrationRequest registrationRequest, string failureMessage)
         {
-            // Add every message to our result.
-            string result = "";
-            for(int i = 0; i < packet.Count; i++)
-            { 
-                if (i == packet.Count - 1)
-                    result += $"\t{packet[i]}";
-                else
-                    result += $"\t{packet[i]}\n";
-            }
-            return result;
+            Log.Error($"[{peer.Service.Name}] client({peer.Address}:{peer.Port}) failed to register game server: \"{failureMessage}\"");
         }
 
         private static void Server_OnServicePacketSent(Core.Server.Services.Service service, Core.Server.Services.Peer sender, Core.Server.Messages.Packet packet)
         {
-            Debug($"[{service.Name}] server->client({sender.Address}:{sender.Port}\n" + getPacketDisplayString(packet));
+            packet.ForEach(p => Log.Debug($"[{service.Name}] ({sender.Address}:{sender.Port}) SENT: " + p));
         }
 
         private static void Server_OnServicePacketReceived(Core.Server.Services.Service service, Core.Server.Services.Peer sender, Core.Server.Messages.Packet packet)
         {
-            Debug($"[{service.Name}] client({sender.Address}:{sender.Port})->server:\n" + getPacketDisplayString(packet));
-        }
-
-
-        private static void Error(string msg)
-        {
-            lock (_printLock)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine(msg);
-                Console.ResetColor();
-            }
-        }
-
-        private static void Warning(string msg)
-        {
-            lock (_printLock)
-            {
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine(msg);
-                Console.ResetColor();
-            }
-        }
-        private static void Info(string msg)
-        {
-            lock (_printLock)
-            {
-                Console.ResetColor();
-                Console.WriteLine(msg);
-            }
-        }
-
-        private static void Debug(string msg)
-        {
-            lock (_printLock)
-            {
-                Console.ForegroundColor = ConsoleColor.DarkCyan;
-                Console.WriteLine(msg);
-                Console.ResetColor();
-            }
+            packet.ForEach(p => Log.Debug($"[{service.Name}] ({sender.Address}:{sender.Port}) RECV: " + p));
         }
     }
 }
